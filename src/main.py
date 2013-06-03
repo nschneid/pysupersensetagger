edec436ed747a3698bb4dfc8debab5a543e33f26
Nathan Schneider <nschneid@cs.cmu.edu>
2013-06-02 20:30:25 -0400

    timeit

--- src/main.py
+++ src/main.py
@@ -3,11 +3,17 @@ Created on Sep 29, 2012
 
 @author: Nathan Schneider (nschneid)
 '''
+from __future__ import absolute_import
+import timeit
+
 import pyximport; pyximport.install()
 import discriminativeTagger
 
+@timeit.Timer
+def go():
+    try:
+        discriminativeTagger.main()
+    except KeyboardInterrupt:
+        raise
 
-try:
-    discriminativeTagger.main()
-except KeyboardInterrupt:
-    raise
+print go.timeit(number=1)
