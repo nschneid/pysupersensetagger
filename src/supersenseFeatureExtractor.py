b6ab4bd818dca4926ba562f5a2aa802404e9eb42
Nathan Schneider <nschneid@cs.cmu.edu>
2013-07-04 20:02:50 -0400

    features combining stem and POS; also look at dice, t, mle values if available

--- src/supersenseFeatureExtractor.py
+++ src/supersenseFeatureExtractor.py
@@ -469,6 +469,8 @@ def extractFeatureValues(sent, j, usePredictedLabels=True, orders={0,1}, indexer
             featureMap["stem"+delta,sent[k].stem] = 1
             featureMap["pos"+delta,sent[k].pos] = 1
             featureMap["cpos"+delta,coarsen(sent[k].pos)] = 1
+            featureMap["stem@0+pos"+delta,sent[j].stem,sent[k].pos] = 1
+            featureMap["stem@0+cpos"+delta,sent[j].stem,coarsen(sent[k].pos)] = 1
             if useClusterFeatures:
                 cluster = wordClusterID(sent[k].token)
                 featureMap["cluster"+delta,cluster] = 1
@@ -491,10 +493,8 @@ def extractFeatureValues(sent, j, usePredictedLabels=True, orders={0,1}, indexer
                 #print(entry,file=sys.stderr)
                 # TODO: check for ordering match against the lexical entry ordering?
                 featureMap["contigMatch,first="+('1' if c==j else '0'),entry["datasource"]] = 1
-                if "count" in entry:
-                    featureMap["contigMatch,first="+('1' if c==j else '0'),entry["datasource"],"count"] = entry["count"]
-                if "pmi" in entry:
-                    featureMap["contigMatch,first="+('1' if c==j else '0'),entry["datasource"],"pmi"] = entry["pmi"]
+                for z in ("count","pmi","t","mle","dice"):
+                    featureMap["contigMatch,first="+('1' if c==j else '0'),entry["datasource"],z] = entry[z]
                 featureMap["contigMatch",entry["datasource"],"poses=",sentpos[c:c+len(entry["lemmas"] if entry.get("lemmas") else entry["words"])],'@',str(j-c)] = 1
                 # TODO: look at label?
                 nContig += 1
@@ -507,10 +507,8 @@ def extractFeatureValues(sent, j, usePredictedLabels=True, orders={0,1}, indexer
                 # TODO: constrain number & placement of gaps to consider this a match?
                 if (entry.get("lemmas") and sent[j].stem in entry["lemmas"]) or (entry.get("words") and sent[j].token in entry["words"]):
                     featureMap["gappyMatch",entry["datasource"]] = 1
-                    if "count" in entry:
-                        featureMap["gappyMatch",entry["datasource"],"count"] = entry["count"]
-                    if "pmi" in entry:
-                        featureMap["gappyMatch",entry["datasource"],"pmi"] = entry["pmi"]
+                    for z in ("count","pmi","t","mle","dice"):
+                        featureMap["gappyMatch",entry["datasource"],z] = entry[z]
                     if "poses" in entry and entry["poses"]:
                         featureMap["gappyMatch",entry["datasource"],"entryposes=",' '.join(entry["poses"]),"cpos=",cposj] = 1
                     nGappy += 1
