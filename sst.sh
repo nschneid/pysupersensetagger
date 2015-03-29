e74ea017e94d4ba6976ba1d0cf6de5e0295eecf5
Nathan Schneider <nschneid@cs.cmu.edu>
2015-03-29 20:41:49 +0100

    fix sst.sh

--- sst.sh
+++ sst.sh
@@ -10,5 +10,5 @@ input=$1 # word and POS tag on each line (tab-separated)
 
 ./predict_sst.sh $input > $input.pred.tags
 
-$pytagger/src/tags2sst.py -l $input.pred.tags > $input.pred.sst
+src/tags2sst.py -l $input.pred.tags > $input.pred.sst
 
